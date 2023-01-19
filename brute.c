27331ac5b43b03d755c2bc997fca9748e576d394
Chance Johnson <chancej715@gmail.com>
2023-01-18 20:56:08 -0600

    Full adder.

--- brute.c
+++ brute.c
@@ -4,6 +4,7 @@ int main(void)
 {
     // Logic gates
     int and(int a, int b) { return a & b; }
+    int or(int a, int b) { return a | b; }
     int xor(int a, int b) { return a ^ b; }
 
     // Half adder
@@ -14,6 +15,7 @@ int main(void)
         int sum = xor(a, b);
         int carry = and(a, b);
 
+        // Output
         half_adder_out[0] = sum;
         half_adder_out[1] = carry;
     }
@@ -24,9 +26,20 @@ int main(void)
     void full_adder(int a, int b, int carry_in)
     {
         // First half adder
-        
+        half_adder(a, b);
+
+        int half_sum_out = half_adder_out[0];
+        int half_carry_out = half_adder_out[1];
 
         // Second half adder
+        half_adder(carry_in, half_sum_out);
+
+        int sum_out = half_adder_out[0];
+        int carry_out = or(half_carry_out, half_adder_out[1]);
+
+        // Output
+        full_adder_out[0] = sum_out;
+        full_adder_out[1] = carry_out;
     }
 
     return 0;
